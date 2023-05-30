2eb8bef00cfbdd8440d343c06c2516a455f5f07b
Nevin Kaplan <n.kaplan@castsoftware.com>
2023-05-30 17:55:11 -0400

    Export to excel

--- gatekeeper.py
+++ gatekeeper.py
@@ -1,7 +1,9 @@
 from cast_common.aipRestCall import AipRestCall
 from cast_common.logger import Logger,INFO
-from pandas import json_normalize
+from cast_common.util import format_table
+from pandas import ExcelWriter
 from argparse import ArgumentParser
+from os.path import abspath
 
 if __name__ == "__main__":
 
@@ -10,22 +12,40 @@ if __name__ == "__main__":
     parser.add_argument('-u','--user',required=True,help='CAST REST API User Name')
     parser.add_argument('-p','--password',required=True,help='CAST REST API Password')
     parser.add_argument('-a','--application',required=True,help='Application Name')
+    parser.add_argument('-f','--healthFactor',required=False,default='60017',help='Health Factor Code')
+    parser.add_argument('-o','--output',required=False,help='Output Folder')
 
     args=parser.parse_args()
 
     log = Logger()
 
-    aip = AipRestCall(args.restURL, args.user, args.password,log_level=INFO,accept_json=True)
-    domain_id = aip.get_domain(args.application)
+#    aip = AipRestCall(args.restURL, args.user, args.password,log_level=INFO,accept_json=True)
+    aip = AipRestCall(args.restURL, args.user, args.password,log_level=INFO)
+    domain_id = aip.get_domain(f'{args.application}_central')
     if domain_id==None:
         log.error(f'Domain not found: {args.application}')
     else:
+        total = 0
+        added = 0
         snapshot_id = aip.get_latest_snapshot(domain_id)['id']
-        df=aip.get_rules(domain_id,snapshot_id,60017,critical=True,non_critical=False,start_row=1,max_rows=999999)
+        df=aip.get_rules(domain_id,snapshot_id,args.healthFactor,critical=True,non_critical=False,start_row=1,max_rows=999999)
         if df.empty:
             log.info(f'No violations found.')
         else:
-            df=json_normalize(df['component'])
-            added=len(df[df['status'] == 'added'])
-            log.info(f'This snapshot added {added} new violations.')
-        exit(0)
+            total=len(df)
+            df=df.loc[df['component.status'] == 'added']
+            added=len(df)
+            detail_df = df[['component.name','component.shortName','rulePattern.name','rulePattern.critical']]
+            detail_df = detail_df.rename(columns={'component.name':'Component Name','component.shortName':'Component Short Name','rulePattern.name':'Rule','rulePattern.critical':'Critical'})
+
+            base='./'
+            if not args.output is None:
+                base = args.output
+
+            file_name = abspath(f'{base}/violations.xlsx')
+            writer = ExcelWriter(file_name, engine='xlsxwriter')
+            cloud_tab = format_table(writer,detail_df,'Violations',[120,50,75,10])
+            writer.close()
+
+            log.info(f'{added} new violations added')
+        exit(added)
