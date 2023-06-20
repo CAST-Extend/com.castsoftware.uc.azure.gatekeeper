2986ef02720f77683956a56aceeff7f2a2dfc9aa
sharath9620 <s.p@castsoftware.com>
2023-06-20 15:15:47 +0530

    Health Table changes for single snapshot application

--- gatekeeper.py
+++ gatekeeper.py
@@ -1,7 +1,7 @@
 from cast_common.aipRestCall import AipRestCall
 from cast_common.logger import Logger,INFO
 from cast_common.util import format_table
-from pandas import ExcelWriter,merge,options, DataFrame
+from pandas import ExcelWriter, Series,merge,options, DataFrame
 from argparse import ArgumentParser
 from os.path import abspath,exists
 from datetime import datetime
@@ -77,69 +77,185 @@ if __name__ == "__main__":
             new_grades=new_grades.drop(unwanted,axis=1).transpose()[['All']].rename(columns={'All':'Latest'})
             
             old_grades = aip.get_grades_by_technology(domain_id,prev_snapshot).drop(unwanted,axis=1).transpose()[['All']].rename(columns={'All':'Previous'})
+
             combined = merge(old_grades,new_grades,left_index=True,right_index=True).reset_index()
             combined['Change'] = combined[['Previous', 'Latest']].pct_change(axis=1)['Latest']
             format_table(writer,combined,'Grades',[50,10,10,10])
+        else:
+            new_grades = aip.get_grades_by_technology(domain_id,snapshot)
+            unwanted=new_grades.columns[new_grades.columns.str.startswith('ISO')]
+            new_grades=new_grades.drop(unwanted,axis=1).transpose()[['All']].rename(columns={'All':'Latest'})
+
+            data = {
+            "Previous": ['N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A']
+            }
+
+            old_grades = DataFrame(data, index =  ['TQI', 'Robustness', 'Efficiency', 'Security', 'Transferability', 'Changeability', 'Documentation'])
+
+            #load data into a DataFrame object:
+            combined = merge(old_grades,new_grades,left_index=True,right_index=True).reset_index()
+
+            combined['Change'] = ['N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A']
+            format_table(writer,combined,'Grades',[50,10,10,10])
 
         #if application does not contains previous snapshot then prev_snapshot['date'] = 0
-        if  len(prev_snapshot) == 0:
+        if len(prev_snapshot) == 0:
             prev_snapshot['date'] = 0
         prev_snapshot_date = prev_snapshot['date']
 
         table_data = ''
         if not combined.empty:
 
-            TQI_style = "text-align: center; color: red" if combined.iloc[0]['Change'] < 0 else "text-align: center; color:green"
-            Robustness_style = "text-align: center; color: red" if combined.iloc[1]['Change'] < 0 else "text-align: center; color:green"
-            Efficiency_style = "text-align: center; color: red" if combined.iloc[2]['Change'] < 0 else "text-align: center; color:green"
-            Security_style = "text-align: center; color: red" if combined.iloc[3]['Change'] < 0 else "text-align: center; color:green"
-            Transferability_style = "text-align: center; color: red" if combined.iloc[4]['Change'] < 0 else "text-align: center; color:green"
-            Changeability_style = "text-align: center; color: red" if combined.iloc[5]['Change'] < 0 else "text-align: center; color:green"
-            Documentation_style = "text-align: center; color: red" if combined.iloc[6]['Change'] < 0 else "text-align: center; color:green"
+            if combined.iloc[0]['Change'] == 'N/A':
+                TQI_style= "text-align: center; {td_style}"
+            else:
+                TQI_style = "text-align: center; color: red" if combined.iloc[0]['Change'] < 0 else "text-align: center; color:green"
+
+            if combined.iloc[1]['Change'] == 'N/A':
+                Robustness_style = "text-align: center; {td_style}"
+            else:    
+                Robustness_style = "text-align: center; color: red" if combined.iloc[1]['Change'] < 0 else "text-align: center; color:green"
+
+            if combined.iloc[2]['Change'] == 'N/A':
+                Efficiency_style = "text-align: center; {td_style}"
+            else:
+                Efficiency_style = "text-align: center; color: red" if combined.iloc[2]['Change'] < 0 else "text-align: center; color:green"
+
+            if combined.iloc[3]['Change'] == 'N/A':
+                Security_style = "text-align: center; {td_style}"
+            else:
+                Security_style = "text-align: center; color: red" if combined.iloc[3]['Change'] < 0 else "text-align: center; color:green"
+
+            if combined.iloc[4]['Change'] == 'N/A':
+                Transferability_style = "text-align: center; {td_style}"
+            else:
+                Transferability_style = "text-align: center; color: red" if combined.iloc[4]['Change'] < 0 else "text-align: center; color:green"
+
+            if combined.iloc[5]['Change'] == 'N/A':
+                Changeability_style = "text-align: center; {td_style}"
+            else:    
+                Changeability_style = "text-align: center; color: red" if combined.iloc[5]['Change'] < 0 else "text-align: center; color:green"
+
+            if combined.iloc[6]['Change'] == 'N/A':
+                Documentation_style = "text-align: center; {td_style}"
+            else:
+                Documentation_style = "text-align: center; color: red" if combined.iloc[6]['Change'] < 0 else "text-align: center; color:green"
             td_style = "padding-left: 15px; padding-right: 5px; padding-top: 3px; padding-bottom: 3px; border-bottom: 1px solid #DDDDDD;"
+
+
+            if combined.iloc[0]['Previous'] == 'N/A':
+                TQI_prev = combined.iloc[0]['Previous']
+            else:
+                TQI_prev = "%.2f" % round(combined.iloc[0]['Previous'], 2)
+
+            if combined.iloc[1]['Previous'] == 'N/A':
+                Robustness_prev = combined.iloc[1]['Previous']
+            else:
+                Robustness_prev = "%.2f" % round(combined.iloc[1]['Previous'], 2)
+
+            if combined.iloc[2]['Previous'] == 'N/A':
+                Efficiency_prev = combined.iloc[2]['Previous']
+            else:
+                Efficiency_prev = "%.2f" % round(combined.iloc[2]['Previous'], 2)
+
+            if combined.iloc[3]['Previous'] == 'N/A':
+                Security_prev = combined.iloc[3]['Previous']
+            else:
+                Security_prev = "%.2f" % round(combined.iloc[3]['Previous'], 2)
+
+            if combined.iloc[4]['Previous'] == 'N/A':
+                Transferability_prev = combined.iloc[4]['Previous']
+            else:
+                Transferability_prev = "%.2f" % round(combined.iloc[4]['Previous'], 2)
+            
+            if combined.iloc[5]['Previous'] == 'N/A':
+                Changeability_prev = combined.iloc[5]['Previous']
+            else:
+                Changeability_prev = "%.2f" % round(combined.iloc[5]['Previous'], 2)
+
+            if combined.iloc[6]['Previous'] == 'N/A':
+                Documentation_prev = combined.iloc[6]['Previous']
+            else:
+                Documentation_prev = "%.2f" % round(combined.iloc[6]['Previous'], 2)
+
+
+            if combined.iloc[0]['Change'] == 'N/A':
+                TQI_change = combined.iloc[0]['Change']
+            else:
+                TQI_change = "%.2f" % round(combined.iloc[0]['Change'], 2)
+
+            if combined.iloc[1]['Change'] == 'N/A':
+                Robustness_change = combined.iloc[1]['Change']
+            else:
+                Robustness_change = "%.2f" % round(combined.iloc[1]['Change'], 2)
+
+            if combined.iloc[2]['Change'] == 'N/A':
+                Efficiency_change = combined.iloc[2]['Change']
+            else:
+                Efficiency_change = "%.2f" % round(combined.iloc[2]['Change'], 2)
+
+            if combined.iloc[3]['Change'] == 'N/A':
+                Security_change = combined.iloc[3]['Change']
+            else:
+                Security_change = "%.2f" % round(combined.iloc[3]['Change'], 2)
+
+            if combined.iloc[4]['Change'] == 'N/A':
+                Transferability_change = combined.iloc[4]['Change']
+            else:
+                Transferability_change = "%.2f" % round(combined.iloc[4]['Change'], 2)
+            
+            if combined.iloc[5]['Change'] == 'N/A':
+                Changeability_change = combined.iloc[5]['Change']
+            else:
+                Changeability_prev = "%.2f" % round(combined.iloc[5]['Change'], 2)
+
+            if combined.iloc[6]['Change'] == 'N/A':
+                Documentation_change = combined.iloc[6]['Change']
+            else:
+                Documentation_change = "%.2f" % round(combined.iloc[6]['Change'], 2)
 
             table_data =f"""
                 <tr>
                     <td style="{td_style}">TQI</td>
-                    <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[0]['Previous'], 2)}</td>
+                    <td style="text-align: center; {td_style}">{TQI_prev}</td>
                     <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[0]['Latest'], 2)}</td>
-                    <td style="{TQI_style}; {td_style}">{"%.2f" % round(combined.iloc[0]['Change'], 2)}</td>
+                    <td style="{TQI_style}; {td_style}">{TQI_change}</td>
                 </tr>
                 <tr>
                     <td style="padding-left: 15px; {td_style}">Robustness</td>
-                    <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[1]['Previous'], 2)}</td>
+                    <td style="text-align: center; {td_style}">{Robustness_prev}</td>
                     <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[1]['Latest'], 2)}</td>
-                    <td style="{Robustness_style}; {td_style}">{"%.2f" % round(combined.iloc[1]['Change'], 2)}</td>
+                    <td style="{Robustness_style}; {td_style}">{Robustness_change}</td>
                 </tr>
                 <tr>
                     <td style="{td_style}">Efficiency</td>
-                    <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[2]['Previous'], 2)}</td>
+                    <td style="text-align: center; {td_style}">{Efficiency_prev}</td>
                     <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[2]['Latest'], 2)}</td>
-                    <td style="{Efficiency_style}; {td_style}">{"%.2f" % round(combined.iloc[2]['Change'], 2)}</td>
+                    <td style="{Efficiency_style}; {td_style}">{Efficiency_change}</td>
                 </tr>
                 <tr>
                     <td style="{td_style}">Security</td>
-                    <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[3]['Previous'], 2)}</td>
+                    <td style="text-align: center; {td_style}">{Security_prev}</td>
                     <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[3]['Latest'], 2)}</td>
-                    <td style="{Security_style}; {td_style}">{"%.2f" % round(combined.iloc[3]['Change'], 2)}</td>
+                    <td style="{Security_style}; {td_style}">{Security_change}</td>
                 </tr>
                 <tr>
                     <td style="{td_style}">Transferability</td>
-                    <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[4]['Previous'], 2)}</td>
+                    <td style="text-align: center; {td_style}">{Transferability_prev}</td>
                     <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[4]['Latest'], 2)}</td>
-                    <td style="{Transferability_style}; {td_style}">{"%.2f" % round(combined.iloc[4]['Change'], 2)}</td>
+                    <td style="{Transferability_style}; {td_style}">{Transferability_change}</td>
                 </tr>
                 <tr>
                     <td style="{td_style}">Changeability</td>
-                    <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[5]['Previous'], 2)}</td>
+                    <td style="text-align: center; {td_style}">{Changeability_prev}</td>
                     <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[5]['Latest'], 2)}</td>
-                    <td style="{Changeability_style}; {td_style}">{"%.2f" % round(combined.iloc[5]['Change'], 2)}</td>
+                    <td style="{Changeability_style}; {td_style}">{Changeability_change}</td>
                 </tr>
                 <tr> 
                     <td style="{td_style}">Documentation</td>
-                    <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[6]['Previous'], 2)}</td>
+                    <td style="text-align: center; {td_style}">{Documentation_prev}</td>
                     <td style="text-align: center; {td_style}">{"%.2f" % round(combined.iloc[6]['Latest'], 2)}</td>
-                    <td style="{Documentation_style}; {td_style}">{"%.2f" % round(combined.iloc[6]['Change'], 2)}</td>
+                    <td style="{Documentation_style}; {td_style}">{Documentation_change}</td>
                 </tr>
                 """
 
