5d44346e4cbdcb35d6af3a6560c01fa732cc8064
sharath9620 <119046105+sharath9620@users.noreply.github.com>
2023-07-03 19:04:33 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,18 +1,13 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
+import os
 import smtplib
 import ssl
 
-def send_email(application):
-    # Define these once; use them twice!
-    SENDER = 's.p@castsoftware.com'
-    RECIPENTS = ['s.p@castsoftware.com', 'b.prakash@castsoftware.com', 's.mahton@castsoftware.com']
-    HOST = 'email-smtp.eu-central-1.amazonaws.com'
-    PORT = 587
-    USERNAME_SMTP = 'AKIAZV4FQR23NGDUIIOI'
-    PASSWORD_SMTP = 'BNcNAA8oGY2cl2VRwufb8hgRyAScUVxc0S6e8c1Z+ewP'
+def send_email(application, SENDER, RECIPENTS, HOST, PORT, USERNAME_SMTP, PASSWORD_SMTP):
 
+    RECIPENTS = eval(RECIPENTS)
     # Create the root message and fill in the from, to, and subject headers
     msgRoot = MIMEMultipart('related')
     msgRoot['Subject'] = f'{application} Application Health'
@@ -28,14 +23,14 @@ def send_email(application):
     msgText = MIMEText('This is the alternative plain text message.')
     msgAlternative.attach(msgText)
 
-    with open('ApplicationHealth.htm', 'r') as myfile:
+    with open(os.getcwd()+'\ApplicationHealth.htm', 'r') as myfile:
         data=myfile.read()
     # We reference the image in the IMG SRC attribute by the ID we give it below
     msgText = MIMEText(data, 'html')
     msgAlternative.attach(msgText)
 
     # This example assumes the image is in the current directory
-    fp = open('img\logo.png', 'rb')
+    fp = open(os.getcwd()+'\img\logo.png', 'rb')
     msgImage = MIMEImage(fp.read())
     fp.close()
 
